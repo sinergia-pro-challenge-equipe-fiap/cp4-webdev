794bdb316532fcc8a98ea006479c4e1b26ba6850
Lucas <abatrlucas@gmail.com>
2025-09-01 17:03:30 -0300

    Criando e estilizando nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import {Link} from 'react-router-dom'
+import Logo from "../assets/imagem-calculadora.png"
 
 const Nav = () => {
   return (
-    <div>
-    
-    </div>
+    <header className="bg-sky-650 text-white p-4 shadow-md">
+        <nav className="container mx-auto flex justify-between items-center">
+             <img src={Logo} alt="logo" className="h-10 w-auto6" /> 
+            <ul className="flex space-x-4">
+                <li className="list-none">
+                    <Link to="/" className="hover:text-blue-200 transition-colors duration 300">Home</Link>
+                </li>
+
+                <li>
+                    <Link to="/calculadora" className="hover:text-blue-200 transition-colors duration 300">Calculadora</Link>
+                </li>
+            </ul>
+        </nav>
+    </header>
   )
 }
 
