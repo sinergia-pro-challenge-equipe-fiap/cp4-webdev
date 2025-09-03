b88fba47ac65296b74d790bb55d7bbee99519fd5
Lucas <abatrlucas@gmail.com>
2025-09-03 10:17:15 -0300

    Criando footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+
+     <footer className=' bg-gray-900 text-white p-6 flex flex-col items-center justify-center space-y-4'>
+      <h3 className="text-sm md:text-2xl">&copy; 2025 - direitos reservados</h3>
+    </footer>
   )
 }
-
 export default Footer
